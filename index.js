c3248e3c438f1b8432be318e314b9f33b3b75b49
Muhammad Ilham Purnama <ilhampurnama78@gmail.com>
2024-05-27 01:06:31 +0700

    Make cart, notif and chat bar interactive

--- index.js
+++ index.js
@@ -130,7 +130,47 @@ function prevProduct() {
   console.log(productIndex);
 }
 
+const cartElement = document.getElementById('cart');
 
+cartElement.addEventListener('mouseover', () => {
+  const cartPreview = document.getElementById('cart-preview');
+  cartPreview.style.transform = "translateY(0px)"
+  cartPreview.style.opacity = "1"
+})
+
+cartElement.addEventListener('mouseout', () => {
+  const cartPreview = document.getElementById('cart-preview');
+  cartPreview.style.transform = "translateY(-400px)"
+  cartPreview.style.opacity = "0.4"
+})
+
+const notificationElement = document.getElementById('notification');
+
+notificationElement.addEventListener('mouseover', () => {
+  const notificationPreview = document.getElementById('notification-preview');
+  notificationPreview.style.transform = "translateY(0px)"
+  notificationPreview.style.opacity = "1"
+})
+
+notificationElement.addEventListener('mouseout', () => {
+  const notificationPreview = document.getElementById('notification-preview');
+  notificationPreview.style.transform = "translateY(-400px)";
+  notificationPreview.style.opacity = "0.4"
+})
+
+const chatElement = document.getElementById('chat');
+
+chatElement.addEventListener('mouseover', () => {
+  const chatPreview = document.getElementById('chat-preview');
+  chatPreview.style.transform = "translateY(0px)"
+  chatPreview.style.opacity = "1"
+})
+
+chatElement.addEventListener('mouseout', () => {
+  const chatPreview = document.getElementById('chat-preview');
+  chatPreview.style.transform = "translateY(-400px)";
+  chatPreview.style.opacity = "0.4"
+})
 
 
 
