17cd0328a95ddf5f671b4b7ee227de00002aefdf
Muhammad Ilham Purnama <ilhampurnama78@gmail.com>
2024-05-27 12:06:17 +0700

    Classify icons image

--- index.js
+++ index.js
@@ -1,3 +1,48 @@
+// Header Icon
+
+const cartElement = document.getElementById('cart');
+
+cartElement.addEventListener('mouseover', () => {
+  const cartPreview = document.getElementById('cart-preview');
+  cartPreview.style.transform = "translateY(0px)"
+  cartPreview.style.opacity = "1"
+})
+
+cartElement.addEventListener('mouseout', () => {
+  const cartPreview = document.getElementById('cart-preview');
+  cartPreview.style.transform = "translateY(-400px)"
+  cartPreview.style.opacity = "0.4"
+})
+
+const notificationElement = document.getElementById('notification');
+
+notificationElement.addEventListener('mouseover', () => {
+  const notificationPreview = document.getElementById('notification-preview');
+  notificationPreview.style.transform = "translateY(0px)"
+  notificationPreview.style.opacity = "1"
+})
+
+notificationElement.addEventListener('mouseout', () => {
+  const notificationPreview = document.getElementById('notification-preview');
+  notificationPreview.style.transform = "translateY(-400px)";
+  notificationPreview.style.opacity = "0.4"
+})
+
+const chatElement = document.getElementById('chat');
+
+chatElement.addEventListener('mouseover', () => {
+  const chatPreview = document.getElementById('chat-preview');
+  chatPreview.style.transform = "translateY(0px)"
+  chatPreview.style.opacity = "1"
+})
+
+chatElement.addEventListener('mouseout', () => {
+  const chatPreview = document.getElementById('chat-preview');
+  chatPreview.style.transform = "translateY(-400px)";
+  chatPreview.style.opacity = "0.4"
+})
+
+
 //Fetch products
 let products = [];
 let productsHTML = '';
@@ -82,6 +127,8 @@ setInterval(function() {
 }
 , 2000)
 
+// Products slider
+
 let  productIndex = 0;
 
 function nextProduct() {
@@ -129,48 +176,6 @@ function prevProduct() {
 
   console.log(productIndex);
 }
-
-const cartElement = document.getElementById('cart');
-
-cartElement.addEventListener('mouseover', () => {
-  const cartPreview = document.getElementById('cart-preview');
-  cartPreview.style.transform = "translateY(0px)"
-  cartPreview.style.opacity = "1"
-})
-
-cartElement.addEventListener('mouseout', () => {
-  const cartPreview = document.getElementById('cart-preview');
-  cartPreview.style.transform = "translateY(-400px)"
-  cartPreview.style.opacity = "0.4"
-})
-
-const notificationElement = document.getElementById('notification');
-
-notificationElement.addEventListener('mouseover', () => {
-  const notificationPreview = document.getElementById('notification-preview');
-  notificationPreview.style.transform = "translateY(0px)"
-  notificationPreview.style.opacity = "1"
-})
-
-notificationElement.addEventListener('mouseout', () => {
-  const notificationPreview = document.getElementById('notification-preview');
-  notificationPreview.style.transform = "translateY(-400px)";
-  notificationPreview.style.opacity = "0.4"
-})
-
-const chatElement = document.getElementById('chat');
-
-chatElement.addEventListener('mouseover', () => {
-  const chatPreview = document.getElementById('chat-preview');
-  chatPreview.style.transform = "translateY(0px)"
-  chatPreview.style.opacity = "1"
-})
-
-chatElement.addEventListener('mouseout', () => {
-  const chatPreview = document.getElementById('chat-preview');
-  chatPreview.style.transform = "translateY(-400px)";
-  chatPreview.style.opacity = "0.4"
-})
 
 
 
