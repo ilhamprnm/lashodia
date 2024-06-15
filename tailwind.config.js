2f8369ab8e504c95e3d50a358c2a859eaab73136
Muhammad Ilham Purnama <ilhampurnama78@gmail.com>
2024-06-15 08:01:54 +0700

    fix margin top, and syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,14 @@ module.exports = {
   theme: {
     extend: {
       fontFamily : {
-        montserrat: "Montserrat, sans-serif,"
+        montserrat: "Montserrat, sans-serif"
       }, 
       boxShadow: {
-        "all-side": "2px 2px 5px rgba(150, 150, 150, 0.8), -2px -2px 5px rgba(150, 150, 150, 0.8), -2px 2px 5px rgba(150, 150, 150, 0.8),2px -2px 5px rgba(150, 150, 150, 0.8) ;",
+        "all-side": "2px 2px 5px rgba(150, 150, 150, 0.8), -2px -2px 5px rgba(150, 150, 150, 0.8), -2px 2px 5px rgba(150, 150, 150, 0.8),2px -2px 5px rgba(150, 150, 150, 0.8)",
   
         "bottom-only" : "0px 2px 6px rgba(123, 123, 123, 0.8)",
   
-        "notif": "1px 1px 2px rgba(150, 150, 150, 0.8), -1px -1px 2px rgba(150, 150, 150, 0.8), -1px 1px 2px rgba(150, 150, 150, 0.8), 1px -1px 2px rgba(150, 150, 150, 0.8) ;",
+        "notif": "1px 1px 2px rgba(150, 150, 150, 0.8), -1px -1px 2px rgba(150, 150, 150, 0.8), -1px 1px 2px rgba(150, 150, 150, 0.8), 1px -1px 2px rgba(150, 150, 150, 0.8)",
   
         "as-thiner" : "1px 1px 1px rgba(170, 170, 170, 0.8), -1px -1px 1px rgba(170, 170, 170, 0.8), -1px 1px 1px rgba(170, 170, 170, 0.8),1px -1px 1px rgba(170, 170, 170, 0.8)"
   
